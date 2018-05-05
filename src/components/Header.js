6ebd073497afc14e2289ef746bf34ae620ad07e2
Wenming W Zhou <wzhou047@nam.ad.pwcinternal.com>
2018-05-05 22:19:23 +0800

    Dynamic data output...

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export default class Header extends Component {
         <div className="container">
           <div className="row">
             <div className="col-xs-1 col-xs-offset-11">
-              <h1>Home</h1>
+              <h1>Header</h1>
             </div>
           </div>
         </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 
 export default class Home extends Component {
   render() {
+    let content = "";
+    if(true){
+      content = "Condition is true caused by 'if' !";
+    }
     return (
         <div className="container">
           <div className="row">
             <div className="col-xs-1 col-xs-offset-11">
-              <h1>Header</h1>
+              <h1>Home</h1>
+              <h3>一、表达式：</h3>
+              <p>{ 1+2 }</p>
+              <p>{ "String" }</p>
+              <p>{ "{表达式}不能输出两行！" }</p>
+              <p>{ content }</p>
+              <p>{ false? "" : "Condition is false caused by '?:' !"}</p>
             </div>
           </div>
         </div>
